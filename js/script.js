6ed2f64905df32d2b7e23d458b4720667f9ae915
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-07 15:46:37 -0700

    add mouseenter and leave functionality

--- js/script.js
+++ js/script.js
@@ -11,7 +11,22 @@ const changeSection = (e) => {
     showSection.classList.remove('show');
     showSection.classList.add('hide');
 
+
+
 }
+
+shareButton.addEventListener("mouseenter", (e) => {
+    const img = e.currentTarget.querySelector('#profile-svg');
+    e.currentTarget.classList.add('profile-share-bg-change');
+    img.classList.add('share-img');
+})
+shareButton.addEventListener("mouseleave", (e) => {
+    const img = e.currentTarget.querySelector('#profile-svg');
+    e.currentTarget.classList.remove('profile-share-bg-change');
+    img.classList.remove('share-img');
+})
 
 shareButton.addEventListener("click", changeSection);
 returnButton.addEventListener('click', changeSection);
+
+
