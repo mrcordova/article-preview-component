790e9ac5dee30095f7eac6780a80332ecf137752
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-07 20:23:48 -0700

    add code for mobile share

--- js/script.js
+++ js/script.js
@@ -4,27 +4,37 @@ const returnButton = document.querySelector('.profile-share-con-click');
 const changeSection = (e) => {
     const hideSection = document.querySelector('.hide');
     const showSection = document.querySelector('.show');
-
-    
-    hideSection.classList.remove('hide');
-    hideSection.classList.add('show');
-    showSection.classList.remove('show');
-    showSection.classList.add('hide');
+    if (isMobile()) {
+      
+        
+        hideSection.classList.remove('hide');
+        hideSection.classList.add('show');
+        showSection.classList.remove('show');
+        showSection.classList.add('hide');
+    } 
 
 
 
 }
 
+function isMobile() {
+  const minWidth = 768; // Minimum width for desktop devices
+  return window.innerWidth < minWidth || screen.width < minWidth;
+}
+
+
+
 shareButton.addEventListener("mouseenter", (e) => {
     const img = e.currentTarget.querySelector('#profile-svg');
-   
     e.currentTarget.classList.add('profile-share-bg-change');
     img.classList.add('share-img');
+    
 })
 shareButton.addEventListener("mouseleave", (e) => {
     const img = e.currentTarget.querySelector('#profile-svg');
     e.currentTarget.classList.remove('profile-share-bg-change');
     img.classList.remove('share-img');
+    
 })
 returnButton.addEventListener("mouseenter", (e) => {
     const img = e.currentTarget.querySelector('#share-svg');
